e8f6232de5a60795f6a0eb5eab1d5539da62fee7
clsteimle94 <codysteizz@gmail.com>
2022-06-05 11:36:15 -0700

    added page gaurd back

--- exec.c
+++ exec.c
@@ -63,9 +63,10 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-
   if((sp = allocuvm(pgdir, KERNBASE - 2*PGSIZE, KERNBASE)) == 0)
     goto bad;
+  clearpteu(pgdir, (char*)(KERNBASE - 2*PGSIZE));
+
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
